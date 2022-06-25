b0a02e9702a15fcd4030cedb656d1f95f067bb48
Ben Letchford <ben@zetifi.com>
2022-06-24 20:42:14 -0400

    wip bottom tabs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { Provider as LocationProvider } from "./state/LocationContext";
 import Map, { ActionBar } from "./components/map";
 import WatchLocation from "./watchers/watchLocation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const MapScreen = () => {
+  return (
+    <>
+      <Map />
+      <ActionBar />
+      <WatchLocation />
+    </>
+  );
+};
 
 export default function App() {
   return (
